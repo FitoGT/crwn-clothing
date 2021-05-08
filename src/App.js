2ab17ec82e561b7e2812eab5ce06b293a2657034
Adolfo <adolfo.guevara.11@gmail.com>
2021-05-08 14:18:21 -0300

    custom button

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Home from './views/home/home'
 import Shop from './views/shop/shop'
+import Sign from './views/sign/sign'
 
+import Header from "./components/header/header"
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/shop" component={Shop} />
         <Route exact path="/" component={Home} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/sign" component={Sign} />
+
       </Switch>
     </div>
   );
---
+++ src/components/form-input/form-input.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './form-input.scss'
+
+const FormInput = ({handleChange, label, ...otherProps}) =>(
+    <div className="group">
+        <input 
+            name={otherProps.name}
+            type={otherProps.name}
+            className="form-input" 
+            onChange={handleChange}
+            value={otherProps.value}
+        />
+        {
+            label ?
+            (
+                <label 
+                     
+                    className={`${otherProps.value.length ? 'shrink':''} form-input-label`}
+                >
+                    {label}
+
+                </label>
+            )
+            : null
+        }
+    </div>
+)
+export default FormInput
