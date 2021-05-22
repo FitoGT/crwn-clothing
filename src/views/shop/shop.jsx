ee2904e40573738e9b7d6c5de149e8362b01a1d6
Adolfo <adolfo.guevara.11@gmail.com>
2021-05-22 13:44:50 -0300

    moving directory and shop to reducers

--- src/views/shop/shop.jsx
+++ src/views/shop/shop.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
-import SHOP_DATA from './shop.data.js'
 import CollectionPreview from '../../components/collection-preview/collection-preview'
-class Shop extends React.Component {
-    constructor(props) {
-        super()
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-    render() {
-        const { collections } = this.state
-        return (
-            <div className="shop-page">
-                {collections.map(({ id, ...collection }) => (
-                    <CollectionPreview key={id} {...collection} />
-                ))}
-            </div>
-        )
-    }
-}
+import { connect } from 'react-redux'
+const Shop = ({collections}) => (
 
-export default Shop
+    <div className="shop-page">
+        {collections.map(({ id, ...collection }) => (
+            <CollectionPreview key={id} {...collection} />
+        ))}
+    </div>
+
+
+)
+
+const mapStateToProps = ({shop}) =>({
+    collections: shop  
+})
+
+export default connect(mapStateToProps)(Shop)
