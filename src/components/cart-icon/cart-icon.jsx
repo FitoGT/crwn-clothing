feba945adbdf36418cf7ceb177ab0297e8bc0c51
Adolfo <adolfo.guevara.11@gmail.com>
2021-05-22 09:45:12 -0300

    selector from the state for counting the items from the cart

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -8,15 +8,19 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.scss'
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden ,itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 
 const mapDispatchToProps = dispatch => ({
-    toggleCartHidden:() => dispatch(toggleCartHidden())
+    toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((accQty,cartItem)=>accQty + cartItem.quantity,0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
